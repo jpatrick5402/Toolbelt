159382d870769c2e850d013fd24590dadaedd6aa
Joseph <jpatrick5402@gmail.com>
2022-12-01 15:22:36 -0500

    Working args into Hello World

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
-int main(){
-	std::cout << "Hello World";
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+	int i;
+	if(argc == 1){
+		std::cout << "Hello, World";
+		return 0;
+	}
+	for (i = 1; i < argc; i++){
+		int res = strcmp(argv[i], "help");
+		if (res == 0){
+			std::cout << "*HELP PAGE*";
+			return 0;
+		}
+		res = strcmp(argv[i], "short");
+		if (res == 0){
+			std::cout << "Hi";
+			return 0;
+		}
+	}
 	return 0;
 }
-
