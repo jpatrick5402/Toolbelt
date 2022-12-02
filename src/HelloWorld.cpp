ea003ac9b54bf59ab948c63205b30abaa6dc458d
jpatrick5402 <jpatrick5402@gmail.com>
2022-12-02 00:41:48 -0500

    Adding more functionality to Hello World

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+
 int main(int argc, char *argv[]){
 	int i;
+	std::string HelpPageString = "Usage:  HelloWorld <Option>\n\nOPTIONS\n\thelp\n\tbreif\n\tyoutube\n";
 	if(argc == 1){
-		std::cout << "Hello, World";
+		std::cout << HelpPageString;
 		return 0;
 	}
 	for (i = 1; i < argc; i++){
 		int res = strcmp(argv[i], "help");
 		if (res == 0){
-			std::cout << "*HELP PAGE*";
+			std::cout << HelpPageString;
 			return 0;
 		}
-		res = strcmp(argv[i], "short");
+		res = strcmp(argv[i], "breif");
 		if (res == 0){
 			std::cout << "Hi";
+			return 0;
+		}
+		res = strcmp(argv[i], "youtube");
+		if (res == 0){
+			std::cout << "https://www.youtube.com/watch?v=Zd53o2PysMA";
 			return 0;
 		}
 	}
